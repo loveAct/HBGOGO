8c8acb5b8df152b30f11b72cce6eaf3d9a596fac
王海波 <haibo.wang@zhangmen.com>
2019-03-23 16:52:45 +0800

    init pro

---
+++ HBGOGO/Classes/HBLSAKManger.h
@@ -0,0 +1,16 @@
+//
+//  HBLSAKManger.h
+//  HBGOGO
+//
+//  Created by 王海波 on 2019/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HBLSAKManger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HBGOGO/Classes/HBLSAKManger.m
@@ -0,0 +1,12 @@
+//
+//  HBLSAKManger.m
+//  HBGOGO
+//
+//  Created by 王海波 on 2019/3/23.
+//
+
+#import "HBLSAKManger.h"
+
+@implementation HBLSAKManger
+
+@end
